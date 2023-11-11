8e8c240d932dfd510284bf5f560b3d7ea7715fd0
Gavin Gao <cggos@outlook.com>
2023-11-11 20:26:46 +0800

    add gym_demo00, gym_demo01

---
+++ rl/gym_demo00.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+from gym import envs
+
+print(envs.registry.keys())
+# print(envs.registry.values())
+
+# env = gym.make("LunarLander-v2", render_mode="human")
+env = gym.make("CarRacing-v2", render_mode="human")
+# env = gym.make("CartPole-v1", render_mode="human")
+
+print(env.action_space)
+print(env.observation_space)
+
+observation, info = env.reset()
+
+timesteps = 1000
+
+for _ in range(timesteps):
+    # print(observation)
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        print("{} timesteps".format(_ + 1))
+        observation, info = env.reset()
+
+env.close()
