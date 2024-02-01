a4f2441ca04dc6b453c097ad7bf2f3ea514ad4ca
Gavin Gao <cggos@outlook.com>
2024-02-01 19:08:02 +0800

    feat: add pytorch_to_onnx.py

---
+++ deep_learning/onnx/pytorch_to_onnx.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import torch
+import onnx
+
+device = torch.device("cuda")
+model = torch.load("/home/ghc/projects/ml/models/yolo/yolov5m-v7.0.pt", map_location=device)['model'].float()
+
+input_names = ["input"]
+output_names = ["output"]
+
+torch.onnx.export(model, input_names, output_names, "model.onnx", opset_version=13, verbose=True)
+
+
