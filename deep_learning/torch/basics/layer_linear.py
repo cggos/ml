6d48f1e19b54788d95c8bce9915178bc48988579
Gavin Gao <cggos@outlook.com>
2024-05-02 23:17:42 +0800

    format layer_linear.py

--- deep_learning/torch/basics/layer_linear.py
+++ deep_learning/torch/basics/layer_linear.py
@@ -4,11 +4,9 @@ import torch.nn as nn
 if __name__ == "__main__":
     in_features = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
 
-    weight_matrix = torch.tensor([
-        [1, 2, 3, 4],
-        [2, 3, 4, 5],
-        [3, 4, 5, 6]
-    ], dtype=torch.float32)
+    weight_matrix = torch.tensor(
+        [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]], dtype=torch.float32
+    )
 
     out_features = weight_matrix.matmul(in_features)
 
@@ -17,5 +15,7 @@ if __name__ == "__main__":
     fc = nn.Linear(4, 3, False)
     fc.weight = nn.Parameter(weight_matrix)
     out_features = fc(in_features)
+
+    print(f'fc.bias: {fc.bias}')
 
     print(f'out_features: {out_features}')
